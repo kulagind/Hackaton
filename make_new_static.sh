41b9313618c0641659eae73bb35bc2d5f73aad12
rgayanov <rgayanov@htsts.ru>
2021-08-21 22:21:51 +0500

    fix script

--- make_new_static.sh
+++ make_new_static.sh
@@ -4,19 +4,19 @@ git checkout master
 
 git pull
 
-cd frontend/ || exit
-
-sudo npm install
-
-npm run build --prod
-
-cd ..
-
-sudo rm -r static/
-
-mkdir static
-
-cp -r frontend/dist/Design/*  static/
+#cd frontend/ || exit
+#
+#sudo npm install
+#
+#npm run build --prod
+#
+#cd ..
+#
+#sudo rm -r static/
+#
+#mkdir static
+#
+#cp -r frontend/dist/Design/*  static/
 
 docker-compose build
 
