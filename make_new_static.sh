21faca4f1cec97ee0efd089dddd89b204d933b03
rgayanov <rgayanov@htsts.ru>
2021-08-21 19:00:57 +0500

    add test bash

--- make_new_static.sh
+++ make_new_static.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-echo" Git checkout master "
 
 git checkout master
 
-echo"Git pull "
 git pull
-
-echo" Go to frontend dir "
 
 cd frontend/ || exit
 
-echo"Npm install"
 sudo npm install
 
-echo"Npm run build"
 npm run build --prod
 
