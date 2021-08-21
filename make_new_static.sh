f9aff69bec77cee98988963a227283c09fc5ca9d
rgayanov <rgayanov@htsts.ru>
2021-08-21 20:21:26 +0500

    fix(master) -fix sh

--- make_new_static.sh
+++ make_new_static.sh
@@ -10,3 +10,16 @@ sudo npm install
 
 npm run build --prod
 
+cd ..
+
+sudo rm -r ststic/
+
+mkdir static
+
+cp -r frontend/dist/Design/*  static/
+
+docker-compose build
+
+docker-compose up -d
+
+
